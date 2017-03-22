b516fa38bd7465f2570d5e387e654c34497f7cb5
Yule Fox <yulefox@gmail.com>
2017-03-22 18:05:35 +0800

    essay

--- c/install.sh
+++ c/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-wget http://dist.schmorp.de/libev/libev-4.24.tar.gz && tar -xf libev-4.24.tar.gz
+#wget http://dist.schmorp.de/libev/libev-4.24.tar.gz && tar -xf libev-4.24.tar.gz
 git clone https://github.com/mreiferson/libevbuffsock.git
 git clone https://github.com/json-c/json-c.git
 git clone https://github.com/nsqio/libnsq.git
