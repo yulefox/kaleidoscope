4d8de6ce94ee9db2c2db8ab400387bf3f6c8c53b
Yunfeng Xiang <yulefox@gmail.com>
2017-05-11 02:42:09 +0000

    1.jpg

--- py/pil.py
+++ py/pil.py
@@ -5,13 +5,6 @@ import os,sys
 from PIL import Image
 
 if __name__ == "__main__":
-    print "__file__=%s" % __file__
-    print "os.path.realpath(__file__)=%s" % os.path.realpath(__file__)
-    print "os.path.dirname(os.path.realpath(__file__))=%s" % os.path.dirname(os.path.realpath(__file__))
-    print "os.path.split(os.path.realpath(__file__))=%s" % os.path.split(os.path.realpath(__file__))[0]
-    print "os.path.abspath(__file__)=%s" % os.path.abspath(__file__)
-    print "os.getcwd()=%s" % os.getcwd()
-    print "sys.path[0]=%s" % sys.path[0]
-    print "sys.argv[0]=%s" % sys.argv[0]
-
     im = Image.open("./img.jpg")
+    im.convert("1")
+    im.save("1.jpg", "JPEG")
