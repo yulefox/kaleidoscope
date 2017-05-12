fbe2f8061dad39bdab458662ce043044e0b292ea
Yule Fox <yulefox@gmail.com>
2017-05-13 00:22:03 +0800

    gitbook-gen

--- py/pil.py
+++ py/pil.py
@@ -1,10 +1,58 @@
 #!/bin/env python
 # -*- encoding=utf8 -*-
 
-import os,sys
+'''
+hello
+'''
+
+#from __future__ import print_function
 from PIL import Image
+import numpy
+from scipy.misc import imsave
+
+def binarize(input_path, output_path, threshold):
+    '''
+    binarize
+    '''
+    image = Image.open(input_path).convert("L")
+    image.load()
+    _h, _w = image.size
+    array = numpy.array(image)
+    print(image.size, image.mode, (len(array), len(array[0])))
+    for i in range(_w):
+        for j in range(_h):
+            if array[i][j] > threshold:
+                array[i][j] = 255
+            else:
+                array[i][j] = 0
+    imsave(output_path, array)
+
+def get_parser():
+    '''
+    Get parser object
+    '''
+    from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+    parser = ArgumentParser(description=__doc__,
+                            formatter_class=ArgumentDefaultsHelpFormatter)
+    parser.add_argument("-i", "--input",
+                        dest="input",
+                        help="read input file",
+                        metavar="FILE",
+                        required=True)
+    parser.add_argument("-o", "--output",
+                        dest="output",
+                        help="write output file",
+                        metavar="FILE",
+                        required=True)
+    parser.add_argument("-t", "--threshold",
+                        dest="threshold",
+                        help="threshold when to show white/black",
+                        default=200,
+                        type=int)
+    return parser
 
 if __name__ == "__main__":
-    im = Image.open("./img.jpg")
-    im.convert("1")
-    im.save("1.jpg", "JPEG")
+    args = get_parser().parse_args()
+    binarize(args.input, args.output, args.threshold)
+    Image.open(args.input).show()
+    Image.open(args.output).show()
